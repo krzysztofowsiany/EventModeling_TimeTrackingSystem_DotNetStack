1e65699cf70304a10b8618ee79ea9a40edd58985
Krzysztof Owsiany <gocom@o2.pl>
2020-05-02 03:48:58 +0200

    fix problem with dockerfile

--- SubmitTimesheet/src/Startup.cs
+++ SubmitTimesheet/src/Startup.cs
@@ -20,19 +20,16 @@ namespace SubmitTimesheet
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
-            // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration => { configuration.RootPath = "ClientApp/dist"; });
 
             services.AddMediatR(Assembly.GetExecutingAssembly());
             services.AddTransient<SaveEventService>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
@@ -42,7 +39,6 @@ namespace SubmitTimesheet
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
@@ -53,9 +49,6 @@ namespace SubmitTimesheet
             app.UseMvc();
             app.UseSpa(spa =>
             {
-                // To learn more about options for serving an Angular SPA from ASP.NET Core,
-                // see https://go.microsoft.com/fwlink/?linkid=864501
-
                 spa.Options.SourcePath = "ClientApp";
 
                 if (env.IsDevelopment())
